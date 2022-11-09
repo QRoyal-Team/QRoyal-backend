95a5080e773c61ac76ce44ce86badbc1cc474de2
PNT <tuong22223@gmail.com>
2022-11-09 22:03:38 +0700

    Desploy fix cors

--- src/main/java/com/hard/qroyal/integration/exception/ExceptionHandler.java
+++ src/main/java/com/hard/qroyal/integration/exception/ExceptionHandler.java
@@ -1,10 +1,14 @@
 package com.hard.qroyal.integration.exception;
 
-import lombok.Data;
+import lombok.*;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import javax.servlet.http.HttpServletRequest;
 import javax.validation.ConstraintViolation;
 import java.util.List;
 import java.util.stream.Collectors;
@@ -20,6 +24,14 @@ public class ExceptionHandler {
 				.map(err -> String.format("'%s' %s", err.getPropertyPath(), err.getMessage()))
 				.collect(Collectors.toList()));
 		return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(dto);
+	}
+
+	@org.springframework.web.bind.annotation.ExceptionHandler(BadCredentialsException.class)
+	@ResponseBody
+	@ResponseStatus(value = HttpStatus.UNAUTHORIZED)
+	private ResponseEntity<?> handleMessageAuth(BadCredentialsException e, HttpServletRequest request) {
+		ErrorDto message = new ErrorDto(HttpStatus.BAD_REQUEST, "Unauthorized");
+		return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(message);
 	}
 
 	@Data
