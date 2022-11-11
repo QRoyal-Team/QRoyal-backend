debc0858331e2ff6902da28de971f2d23d58dd01
PNT <tuong22223@gmail.com>
2022-11-11 15:16:37 +0700

    log time

--- src/main/java/com/hard/qroyal/integration/controllers/client/order/OrderController.java
+++ src/main/java/com/hard/qroyal/integration/controllers/client/order/OrderController.java
@@ -107,8 +107,10 @@ public class OrderController extends BaseController<OrderService, OrderMapper> {
 		SimpleDateFormat formatter = new SimpleDateFormat("yyyyMMddHHmmss");
 		String vnp_CreateDate = formatter.format(cld.getTime());
 		vnp_Params.put("vnp_CreateDate", vnp_CreateDate);
+		log.info(vnp_CreateDate);
 		cld.add(Calendar.MINUTE, 15);
 		String vnp_ExpireDate = formatter.format(cld.getTime());
+		log.info(vnp_ExpireDate);
 		vnp_Params.put("vnp_ExpireDate", vnp_ExpireDate);
 		List fieldNames = new ArrayList(vnp_Params.keySet());
 		Collections.sort(fieldNames);
